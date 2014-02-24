ffd834534ddc8e867240827349379b4deed0e338
Griffin Mahoney <griffin.mahoney@gmail.com>
2014-02-23 23:13:37 -0500

    Fixed date bug

--- app/controllers/event_instances_controller.rb
+++ app/controllers/event_instances_controller.rb
@@ -1,14 +1,20 @@
 class EventInstancesController < ApplicationController
 
   def index
-    start_date ||= Date.parse(params[:start_date])
-    end_date ||= Date.parse(params[:end_date])
+    start_date = params[:start_date]
+    end_date = params[:end_date]
     event_id = params[:event_id]
 
-    if start_date.nil? || end_date.nil?
+    if start_date.empty? || end_date.empty?
       event_instances = EventInstance.where(event_id: event_id)
     else
-      event_instances = EventInstance.where(event_id: event_id, created_at: start_date..end_date)
+      start_date = Date.parse(start_date)
+      end_date = Date.parse(end_date)
+      end_date = end_date + 2.days
+      event_instances = EventInstance.where(
+        event_id: event_id, 
+        created_at: start_date..end_date
+      )
     end
 
     render json: event_instances
